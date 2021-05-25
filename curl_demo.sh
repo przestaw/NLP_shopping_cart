5401c089f7a0693deeecf70e5a58793ebc92354c
przestaw <przemek.stawczyk@gmail.com>
2021-05-25 18:17:35 +0200

    minor demo improvement

--- curl_demo.sh
+++ curl_demo.sh
@@ -5,10 +5,8 @@ curl -s -X GET localhost:5000/info
 
 sleep 0.2
 
-echo '# product with id 182'
+echo '# product with id 175'
 curl -s -X GET localhost:5000/product/175
-
-sleep 0.2
 
 for cart in \
   '["10 Sweet sweet butter", "Some fresh green peas", "five sweet brownie"]' \
@@ -16,10 +14,14 @@ for cart in \
   '["A dozen organic eggs", "1.5l of skimmed milk", "3 kg of flour"]'\
   '["La La Land on dvd", "That movie about kidnapped girls trying to escape", "some popcorn", "Guardians of the galaxy the movie"]'
 do
-  echo "# get prediction for list : $cart"
+  echo "# Current shopping list :"
+  echo -e "\t $cart"
+  echo "# Press any key to send request"
+  read -r -n1 key
+
+  echo "# prediction for list : $cart"
   curl -s --header "Content-Type: application/json" \
     --request POST  \
     --data "{\"shoppingList\": $cart }" \
     http://localhost:5000/cart
-  sleep 0.2
 done
